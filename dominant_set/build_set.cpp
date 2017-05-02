8960a2318797b82fa939b4a81a1623e426d8cd27
Caíque Araújo Spósito <kiqaps@gmail.com>
2017-05-02 11:13:55 -0300

    novo minimal dominant set

--- dominant_set/build_set.cpp
+++ dominant_set/build_set.cpp
@@ -1,1 +1,56 @@
-// TODO
+#include <iostream>
+#include <cstdio>
+#include "AdjList.h"
+
+using namespace std;
+
+set<int> build_dominant_set(AdjList graph);
+
+int main()
+{
+    int order, input;
+
+    cin >> order;
+    AdjList graph(order);
+
+    for (int i = 0; i < order; i++)
+    {
+        for (int j = 0; j < order; j++)
+        {
+            cin >> input;
+            if (input)
+                graph[i].insert(j);
+        }
+    }
+
+    set<int> dominant_set = build_dominant_set(graph);
+    cout << "Minimal Dominant Set = {";
+    for (set<int>::iterator it = dominant_set.begin(); it != dominant_set.end(); it++)
+        cout << " " << *it;
+    cout << " }" << endl;
+    return 0;
+}
+
+set<int> build_dominant_set(AdjList graph)
+{
+    set<int> dominant_set;
+    int maximum_degree;
+    map< int, set<int> >::iterator it;
+
+    while (graph.getOrder())
+    {
+        maximum_degree = graph.getMaximumDegree();
+        for (it = graph.begin(); it != graph.end(); it++)
+        {
+            if (graph.getVertexDegree(it->first) == maximum_degree)
+                break;
+        }
+
+        dominant_set.insert(it->first);
+
+        for (set<int>::iterator it2 = (it->second).begin(); it2 != (it->second).end(); it2++)
+            graph.removeVertex(*it2);
+        graph.removeVertex(it->first);
+    }
+    return dominant_set;
+}
--- dominant_set/check_set_minimal.cpp
+++ dominant_set/check_set_minimal.cpp
@@ -1,1 +1,69 @@
-// TODO
+#include <bits/stdc++.h>
+#include "./AdjMatrix.h"
+
+using namespace std;
+
+bool is_dominant_set(AdjMatrix g, set<int> the_set);
+bool is_minimal_dominant_set(AdjMatrix g, set<int> the_set);
+
+int main()
+{
+    int new_set_member, n_vertex;
+    set<int> dominant_set;
+
+    cin >> n_vertex;
+
+    AdjMatrix graph(n_vertex);
+
+    for (int i = 0; i < n_vertex; i++)
+        for (int j = 0; j < n_vertex; j++)
+            cin >> graph[i][j];
+
+    cin >> new_set_member;
+    while (new_set_member >= 0 && new_set_member < n_vertex)
+    {
+        dominant_set.insert(new_set_member);
+        cin >> new_set_member;
+    }
+
+    cout << is_minimal_dominant_set(graph, dominant_set) << endl;
+    return 0;
+}
+
+bool is_minimal_dominant_set(AdjMatrix g, set<int> the_set)
+{
+    if (!is_dominant_set(g, the_set))
+        return false;
+
+    for (set<int>::iterator it = the_set.begin(); it != the_set.end(); it++)
+    {
+        set<int> new_set;
+        for (set<int>::iterator it2 = the_set.begin(); it2 != the_set.end(); it2++)
+            if (it2 != it)
+                new_set.insert(*it2);
+        if (is_dominant_set(g, new_set))
+            return false;
+    }
+
+    return true;
+}
+
+bool is_dominant_set(AdjMatrix g, set<int> the_set)
+{
+    vector<bool> vertexes = vector<bool>(g.getOrder(), false);
+
+    for (set<int>::iterator it = the_set.begin(); it != the_set.end(); ++it)
+    {
+        vertexes[*it] = true;
+        for (int i = 0; i < g.getOrder(); i++)
+            if (g[*it][i] == 1)
+                vertexes[i] = true;
+    }
+
+    for (int i = 0; i < g.getOrder(); i++)
+    {
+        if (!vertexes[i])
+            return false;
+    }
+    return true;
+}

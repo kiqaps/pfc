23d195646e5e01828ddd36456b5a9ff896683a18
Caíque <kiqaps@gmail.com>
2017-11-14 15:48:39 -0200

    começo dominating

--- src/bruteforce.cpp
+++ src/bruteforce.cpp
@@ -2,6 +2,45 @@
 
 using namespace std;
 
+
+bool is_dominant_set(AdjMatrix g, set<int> the_set)
+{
+    vector<bool> vertexes = vector<bool>(g.getOrder(), false);
+
+    for (set<int>::iterator it = the_set.begin(); it != the_set.end(); ++it)
+    {
+        vertexes[*it] = true;
+        for (int i = 0; i < g.getOrder(); i++)
+            if (g[*it][i] == 1)
+                vertexes[i] = true;
+    }
+
+    for (int i = 0; i < g.getOrder(); i++)
+    {
+        if (!vertexes[i])
+            return false;
+    }
+    return true;
+}
+
+bool is_dominating_set(AdjList g, vector<int> vertices)
+{
+    vector<bool> dominated = vector<bool>(g.ordem(), false);
+    for (int i = 0; i < vertices.size(); i++)
+    {
+        vector<int> viz = vizinhancaFechada(vertices[i]);
+        for (int j = 0; j < viz.size(); j++)
+            dominated[j] = true;
+    }
+
+    for (int i = 0; i < g.ordem(); i++)
+    {
+        if (!dominated[i])
+            return false;
+    }
+    return true;
+}
+
 bool is_independent_set(AdjList g, vector<int> vertices)
 {
     bool is_independent_set = true;
